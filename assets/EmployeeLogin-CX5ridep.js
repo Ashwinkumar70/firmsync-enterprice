0075dd9a6850446f337a625eb11f4c005d0a8e36
Ashwin <ashwinkumarc30@gmail.com>
2026-04-18 22:36:02 +0530

    Updates

---
+++ assets/EmployeeLogin-CX5ridep.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-06mAmuQB.js";import"./vendor-react-D2X5qBQ4.js";import{R as t}from"./RoleLoginPage-DpMMt6al.js";import"./vendor-supabase-CpRTRdvQ.js";import"./vendor-forms-obxyCKQf.js";import"./zod-CFk8yibh.js";import"./vendor-icons-DGvwloeg.js";const a=()=>o.jsx(t,{portalRole:"employee",title:"Employee Portal",showSignup:!0});export{a as EmployeeLogin};
---
+++ assets/HRLogin-CculzLa1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-06mAmuQB.js";import"./vendor-react-D2X5qBQ4.js";import{R as t}from"./RoleLoginPage-DpMMt6al.js";import"./vendor-supabase-CpRTRdvQ.js";import"./vendor-forms-obxyCKQf.js";import"./zod-CFk8yibh.js";import"./vendor-icons-DGvwloeg.js";const l=()=>o.jsx(t,{portalRole:"hr",title:"HR Portal",showSignup:!1});export{l as HRLogin};
